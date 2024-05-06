87725919e3a08bec1d635142ecb22a23e32888b4
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-06 19:15:18 -0400

    installed mongoose and connected it to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {PORT, mongoDbUrl} from './config.js';
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -7,6 +8,15 @@ app.get('/', (req, res) => {
     return res.status(234).send("Welcome to MERN Stack tutorial");
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`);
-});
+
+
+mongoose.connect(mongoDbUrl)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
