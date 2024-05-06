74dc07241c7b842be08329bb3e4ee64d8e3a8733
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-06 18:28:56 -0400

    set up port for development and got mongodburl in a variable

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDbUrl = "mongodb+srv://root:root@book-store-mern.tcxflzw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store-MERN";
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express";
-import {PORT} from './config.js';
+import {PORT, mongoDbUrl} from './config.js';
 
 const app = express();
+
+app.get('/', (req, res) => {
+    return res.status(234).send("Welcome to MERN Stack tutorial");
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
