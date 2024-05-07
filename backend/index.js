956e1365365f1ac578a668681d7bfd086ca947be
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-06 20:11:02 -0400

    made route to save a new book to the db
    tested it with postman

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
 import express from "express";
 import {PORT, mongoDbUrl} from './config.js';
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+//middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.status(234).send("Welcome to MERN Stack tutorial");
 });
 
+// ROute for save a new book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+    } catch(error) {
+        console.log(error);
+        response.status(500).send({message: error.message});
+    }
+});
 
 
 mongoose.connect(mongoDbUrl)
