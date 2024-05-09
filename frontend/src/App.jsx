72de830822a1e648e5e7ef2d4c550705a3bb5a87
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-08 20:00:45 -0400

    added react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
+import CreateBooks from './pages/CreateBooks'
 
 const App = () => {
   return (
-    <div className="bg-red-400 text-white">App</div>
+    <Routes>
+      <Route path='/' element={<Home />} ></Route>
+      <Route path='/books/create' element={<CreateBooks />} ></Route>
+      <Route path='/books/details/:id' element={<ShowBook />} ></Route>
+      <Route path='/books/edit/:id' element={<EditBook />} ></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook />} ></Route>
+    </Routes>
   )
 }
 
